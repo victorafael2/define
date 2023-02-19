6209111f01fc50b8674cd83a046b4202257ed1c0
Victor Rafael de Oliveira Silva <victorafaeloliveira@gmail.com>
2023-02-19 15:52:10 -0300

    2

---
+++ forms/form_1.php
@@ -0,0 +1,51 @@
+<div class="d-flex flex-center content-min-h">
+
+
+
+
+
+
+    <div class="card shadow-none border border-300 my-4" data-component-card="data-component-card">
+        <div class="card-header p-4 border-bottom border-300 bg-soft">
+            <div class="row g-3 justify-content-between align-items-center">
+                <div class="col-12 col-md">
+                    <h4 class="text-900 mb-0" data-anchor="data-anchor" id="checks">Checks<a class="anchorjs-link "
+                            aria-label="Anchor" data-anchorjs-icon="#" href="#checks"
+                            style="padding-left: 0.375em;"></a></h4>
+                </div>
+                <div class="col col-md-auto">
+                    <nav class="nav nav-underline justify-content-end doc-tab-nav align-items-center" role="tablist">
+                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="card-body p-0">
+            
+            <div class="p-4 code-to-copy">
+                <div class="form-check">
+
+                    <input class="form-check-input" id="flexCheckDefault" type="checkbox" value="">
+
+                    <label class="form-check-label" for="flexCheckDefault">Default checkbox</label>
+                </div>
+                <div class="form-check">
+
+                    <input class="form-check-input" id="flexCheckChecked" type="checkbox" value="" checked="">
+
+                    <label class="form-check-label" for="flexCheckChecked">Checked checkbox</label>
+                </div>
+            </div>
+
+            <form action="">
+        <label for="Peso">A Tecnologia a ser desenvolvida é uma Inovação:</label><br>
+        <input type="radio" name="Peso" value="2" required>Para minha empresa, já existe no mercado<br>
+        <input type="radio" name="Peso" value="4" required>Para meu Estado<br>
+        <input type="radio" name="Peso" value="4" required>Para meu país<br>
+        <input type="radio" name="Peso" value="5" required>Inovação mundial<br>
+        <input type="submit" value="Enviar">
+    </form>
+        </div>
+    </div>
+
+</div>
