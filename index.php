87f2d6dc21c764956a7863e7ab3e98cf7111d6f2
victorafael2 <victorafaeloliveira@gmail.com>
2023-05-03 13:25:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ h1 .typewriter {
                                             Seu navegador não suporta a tag de vídeo.
                                         </video> -->
                                     <!-- <iframe  src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                                        <video class="w-100 shadow-lg d-dark-none rounded-2" width="640" height="360" autoplay loop >
+                                        <video class="w-100 shadow-lg d-dark-none rounded-2" src="assets/videos/define.mp4" width="640" height="360" autoplay loop >
                                             Seu navegador não suporta a tag de vídeo.
                                         </video>
                                         <video class="w-100 shadow-lg d-light-none rounded-2" src="assets/videos/define.mp4" width="640" height="360" autoplay loop >
